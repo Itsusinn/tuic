2b2684c2a6ff9b0944978180e22ee41b6de1f07f
iHsin <sun@ihsin.dev>
2025-11-17 14:39:49 +0800

    wip: placeholder for sniff support

--- tuic-core/src/lib.rs
+++ tuic-core/src/lib.rs
@@ -24,4 +24,4 @@ pub mod quinn;
 
 // Utility types
 mod utils;
-pub use self::utils::{CongestionControl, StackPrefer, UdpRelayMode, is_private_ip};
+pub use self::utils::{CongestionControl, StackPrefer, UdpRelayMode, is_private_ip, sniff_from_stream};
